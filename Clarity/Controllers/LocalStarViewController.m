b71dbb44ff29e52d413f310e0d09c2815ccf1c6b
lovejun <hyun2012@gmail.com>
2014-08-29 13:11:19 +0900

    로컬 스타 뷰 동기화 done

--- Clarity/Controllers/LocalStarViewController.m
+++ Clarity/Controllers/LocalStarViewController.m
@@ -12,7 +12,6 @@
 
 
 #import "LocalStarViewController.h"
-//#import "FRLayeredNavigationController/FRLayeredNavigation.h"
 #import "AppDelegate.h"                                     //AppDelegate 참조
 #import "NoteDataManager.h"                                 //노트 데이터 매니저
 #import "LocalNote.h"                                       //노트 데이터 모델
@@ -26,7 +25,6 @@
 @property (nonatomic, weak) IBOutlet UITableView *tableView;                        //테이블 뷰
 @property (nonatomic, weak) IBOutlet UISearchBar *searchBar;                        //서치바
 @property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController; //fetchedResultsController
-@property (nonatomic, strong) UIBarButtonItem *buttonDone;                          //우측 Done 버튼
 @property (nonatomic, strong) NSMutableArray *searchResultNotes;                    //서치바 검색결과를 담을 뮤터블 배열
 @property (nonatomic, strong) LocalNote *selectedNote;                              //AddEdit View로 넘겨 줄 노트
 @property (nonatomic, strong) UIButton *infoButton;                                 //인포 버튼
@@ -45,17 +43,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-//    self.layeredNavigationController.delegate = self;
     [self configureViewAndTableView];
     [self addNavigationBarButtonItem];                                              //내비게이션 바 버튼
     [self hideSearchBar];                                                           //서치바 감춤
-    [self saveCurrentView];                                                         //현재 뷰 > 유저 디폴트 저장
+    [self addObserverForStarListViewWillShow];
 }
 
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    self.title = @"Starred";
     [self showStatusBar];
     [self showNavigationBar];
     [self executePerformFetch];                                                     //패치 코어데이터 아이템
@@ -63,24 +61,23 @@
     [self.tableView reloadData];                                                    //테이블 뷰 업데이트
     [self performUpdateInfoButton];                                                 //업데이트 인포
     [self performCheckNoNote];                                                      //노트 없으면 헬프 레이블 보여줄 것
+    [self saveCurrentView];                                                         //현재 뷰 > 유저 디폴트 저장
+}
+
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
 }
 
 
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:YES];
+    self.title = @"";
+    [self cancelCurrentView];                   //현재 뷰 > 유저 디폴트 캔슬
     [self deActivateSearchDisplayController];   //서치 디스플레이 컨트롤러 비활성화 (애드에딧 뷰에서 나올때 서치 디스플레이 컨트롤러를 거치지 않고 테이블뷰로 바로 돌아옴)
     _fetchedResultsController = nil;
-}
-
-
-#pragma mark 유저 디폴트 > 현재 뷰 저장
-
-- (void)saveCurrentView
-{
-    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
-    [standardUserDefaults setBool:YES forKey:kCURRENT_VIEW_IS_LOCAL];              //현재 뷰
-    [standardUserDefaults synchronize];
 }
 
 
@@ -107,10 +104,9 @@
 
 - (void)configureViewAndTableView
 {
-    self.title = @"Starred";
-    self.view.backgroundColor = kTOOLBAR_DROPBOX_LIST_VIEW_BACKGROUND_COLOR;          //뷰
-    self.tableView.backgroundColor = kCLEAR_COLOR;                                    //테이블 뷰 배경 색상
-    self.tableView.separatorColor = [UIColor colorWithRed:0.333 green:0.333 blue:0.333 alpha:0.1]; //구분선 색상
+    self.view.backgroundColor = kTEXTVIEW_BACKGROUND_COLOR;//kTOOLBAR_DROPBOX_LIST_VIEW_BACKGROUND_COLOR;          //뷰
+    self.tableView.backgroundColor = kTABLE_VIEW_BACKGROUND_COLOR;                                    //테이블 뷰 배경 색상
+    self.tableView.separatorColor = kTEXTVIEW_BACKGROUND_COLOR; //[UIColor colorWithRed:0.333 green:0.333 blue:0.333 alpha:0.1]; //구분선 색상
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
 }
@@ -237,7 +233,6 @@
 
 
 #pragma mark 델리게이트 메소드
-
 #pragma mark 섹션 헤더 속성
 
 - (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section
@@ -333,7 +328,6 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //스토리보드 방식
     LocalAddEditViewController *controller = (LocalAddEditViewController *)[self.storyboard instantiateViewControllerWithIdentifier:@"LocalAddEditViewController"];
     
     if (tableView == self.searchDisplayController.searchResultsTableView) 
@@ -357,8 +351,8 @@
 //        [managedObjectContext setParentContext:[NoteDataManager sharedNoteDataManager].managedObjectContext];
         
         self.selectedNote = (LocalNote *)[managedObjectContext objectWithID:[[self.fetchedResultsController objectAtIndexPath:indexPath] objectID]];
-        //위 코드와 결과 동일
-//        self.selectedNote = (LocalNote *)[self.fetchedResultsController objectAtIndexPath:indexPath];
+        
+//        self.selectedNote = (LocalNote *)[self.fetchedResultsController objectAtIndexPath:indexPath]; //위 코드와 결과 동일
         [controller note:self.selectedNote inManagedObjectContext:managedObjectContext];
         
         controller.isSearchResultNote = NO;
@@ -369,6 +363,26 @@
     }
     
     [self.navigationController pushViewController:controller animated:YES];
+}
+
+
+#pragma mark - 유저 디폴트
+
+#pragma mark 유저 디폴트 > 현재 뷰 저장
+
+- (void)saveCurrentView
+{
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    [standardUserDefaults setBool:YES forKey:kCURRENT_VIEW_IS_LOCAL];                         //현재 뷰
+    [standardUserDefaults synchronize];
+}
+
+
+- (void)cancelCurrentView
+{
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    [standardUserDefaults setBool:NO forKey:kCURRENT_VIEW_IS_LOCAL];                          //현재 뷰
+    [standardUserDefaults synchronize];
 }
 
 
@@ -402,7 +416,8 @@
 {
     NSError *error = nil;
     
-    if (![[self fetchedResultsController] performFetch:&error]) {
+    if (![[self fetchedResultsController] performFetch:&error])
+    {
         NSLog (@"executePerformFetch > error occurred");
         //abort();
     } else {
@@ -480,7 +495,67 @@
 }
 
 
-#pragma mark 내비게이션 및 상태 바 컨트롤
+#pragma mark - 바 버튼 및 메소드
+
+- (void)addNavigationBarButtonItem
+{
+    UIBarButtonItem *barButtonItemFixed = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+    barButtonItemFixed.width = 20.0f;
+    
+    
+    UIView* infoButtonView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 40)];
+    self.infoButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.infoButton.backgroundColor = [UIColor clearColor];
+    self.infoButton.frame = infoButtonView.frame;
+    [self.infoButton setTitle:@"" forState:UIControlStateNormal];
+    self.infoButton.tintColor = kINFOBUTTON_TEXTCOLOR;
+    self.infoButton.autoresizesSubviews = YES;
+    self.infoButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin;
+    [self.infoButton addTarget:self action:@selector(noAction:) forControlEvents:UIControlEventTouchUpInside];
+    self.infoButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
+    self.infoButton.contentEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 2);
+    [infoButtonView addSubview:self.infoButton];
+    UIBarButtonItem* barButtonItemInfo = [[UIBarButtonItem alloc]initWithCustomView:infoButtonView];
+    
+    self.navigationItem.rightBarButtonItems = @[barButtonItemInfo];
+}
+
+
+#pragma mark 버튼 액션 메소드
+
+- (void)noAction:(id)sender
+{
+    
+}
+
+
+#pragma mark 업데이트 인포 버튼
+
+- (void)performUpdateInfoButton
+{
+    [self performSelector:@selector(updateInfoButton) withObject:self.infoButton afterDelay:0.5];
+}
+
+
+- (void)updateInfoButton
+{
+    _totalNotes = (int)[[_fetchedResultsController fetchedObjects] count];        //노트 갯수
+    
+    if (_totalNotes == 0) {
+        [self.infoButton setTitle:@"" forState:UIControlStateNormal];
+    }
+    else if (_totalNotes == 1)
+    {
+        [self.infoButton setTitle:@"1 note" forState:UIControlStateNormal];
+    }
+    else if (_totalNotes > 1)
+    {
+        [self.infoButton setTitle:[NSString stringWithFormat:@"%d notes", _totalNotes] forState:UIControlStateNormal];
+    }
+}
+
+
+#pragma mark - 내비게이션 및 상태 바 컨트롤
 
 - (void)showNavigationBar
 {
@@ -507,7 +582,6 @@
 
 
 #pragma mark - 서치바 및 서치 디스플레이 컨트롤러
-
 #pragma mark 뷰가 처음 나타날 때 서치바 감출지 여부 check
 
 - (void)hideSearchBar
@@ -587,66 +661,6 @@
 }
 
 
-#pragma mark - 내비게이션 바 버튼 및 메소드
-
-- (void)addNavigationBarButtonItem
-{
-    UIBarButtonItem *barButtonItemFixed = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
-    barButtonItemFixed.width = 20.0f;
-    
-    
-    UIView* infoButtonView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 40)];
-    self.infoButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    self.infoButton.backgroundColor = [UIColor clearColor];
-    self.infoButton.frame = infoButtonView.frame;
-    [self.infoButton setTitle:@"" forState:UIControlStateNormal];
-    self.infoButton.tintColor = kINFOBUTTON_TEXTCOLOR;
-    self.infoButton.autoresizesSubviews = YES;
-    self.infoButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin;
-    [self.infoButton addTarget:self action:@selector(noAction:) forControlEvents:UIControlEventTouchUpInside];
-    self.infoButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
-    self.infoButton.contentEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 2);
-    [infoButtonView addSubview:self.infoButton];
-    UIBarButtonItem* barButtonItemInfo = [[UIBarButtonItem alloc]initWithCustomView:infoButtonView];
-    
-    self.navigationItem.rightBarButtonItems = @[barButtonItemInfo];
-}
-
-
-#pragma mark 버튼 액션 메소드
-
-- (void)noAction:(id)sender
-{
-    
-}
-
-
-#pragma mark 업데이트 인포 버튼
-
-- (void)performUpdateInfoButton
-{
-    [self performSelector:@selector(updateInfoButton) withObject:self.infoButton afterDelay:0.5];
-}
-
-
-- (void)updateInfoButton
-{
-    _totalNotes = (int)[[_fetchedResultsController fetchedObjects] count];        //노트 갯수
-    
-    if (_totalNotes == 0) {
-        [self.infoButton setTitle:@"" forState:UIControlStateNormal];
-    }
-    else if (_totalNotes == 1)
-    {
-        [self.infoButton setTitle:@"1 note" forState:UIControlStateNormal];
-    }
-    else if (_totalNotes > 1)
-    {
-        [self.infoButton setTitle:[NSString stringWithFormat:@"%d notes", _totalNotes] forState:UIControlStateNormal];
-    }
-}
-
-
 #pragma mark - 헬프 레이블
 
 - (void)performCheckNoNote
@@ -660,10 +674,39 @@
     if ([[_fetchedResultsController fetchedObjects] count] == 0)
     {
         self.helpLabel.alpha = 1.0;
+        self.tableView.separatorColor = kCLEAR_COLOR;
     }
     else
     {
         self.helpLabel.alpha = 0.0;
+        self.tableView.separatorColor = kCLEAR_COLOR; //kTEXTVIEW_BACKGROUND_COLOR;
+    }
+}
+
+
+#pragma mark - StarListViewWillShow Notification 옵저버 등록
+
+- (void)addObserverForStarListViewWillShow
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didReceiveStarListViewWillShowNotification:)
+                                                 name:@"StarListViewWillShowNotification"
+                                               object:nil];
+}
+
+
+#pragma mark StarListViewWillShow 노티피케이션 수신 후 후속작업
+
+- (void)didReceiveStarListViewWillShowNotification:(NSNotification *)notification
+{
+    if ([[notification name] isEqualToString:@"StarListViewWillShowNotification"])
+    {
+        [self executePerformFetch];                                                     //패치 코어데이터 아이템
+        [self initializeSearchResultNotes];                                             //서치 results 초기화
+        [self.tableView reloadData];                                                    //테이블 뷰 업데이트
+        [self performUpdateInfoButton];                                                 //업데이트 인포
+        [self performCheckNoNote];                                                      //노트 없으면 헬프 레이블 보여줄 것
+        [self saveCurrentView];                                                         //현재 뷰 > 유저 디폴트 저장
     }
 }
 
@@ -683,9 +726,6 @@
 
 - (void)didReceiveMemoryWarning
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];     //Remove 옵저버
-    _fetchedResultsController = nil;                                //fetchedResultsController
-    self.searchResultNotes = nil;                                   //검색결과를 담을 뮤터블 배열
     [super didReceiveMemoryWarning];
 }
 
